6805014bb6839fde7c5ca0e587a9a77a8e0ee807
ZSteam77 <61356101+ZSteam77@users.noreply.github.com>
2023-06-25 21:10:40 +0800

    version update for asynchronous input for sender

--- address_sender.v
+++ address_sender.v
@@ -27,47 +27,52 @@ module address_sender(
     input [`MEM_ADDR_SIZE-1:0] addr_in,
     output reg [`BANDWIDTH_WRITE_ADDRESS-1:0] addr_bus,
     output reg send_addr, // to receiver module, 1 tells the receiver to start receiving
-    output reg done // to buffer or memory, 1 to indicate transmission finishs.
+    output reg done_addr // to buffer or memory, 1 to indicate transmission finishs.
 );
     reg [10:0] send_progress;
-    reg [5:0] count;
+    reg [`MEM_ADDR_SIZE-1:0] address_in;
     integer i,j;
     initial begin
         addr_bus = 0;
         send_addr = 0;
-        done = 0;
+        done_addr = 0;
         send_progress = 0;
-        count = 0;
+        address_in = 0;
+    end
+    always@(addr_in)begin
+        address_in = addr_in;
     end
     always@(posedge clk) begin
         if (reset) begin
+            addr_bus = 0;
             send_addr = 0;
-            done = 0;
+            done_addr = 0;
             send_progress = 0;
+            address_in = 0;
         end
         else begin
             if (send) begin
                 if($signed(`MEM_ADDR_SIZE-(send_progress+1)*`BANDWIDTH_WRITE_ADDRESS) >= 0) begin
                     send_addr = 1;
-                    addr_bus = addr_in[(send_progress*`BANDWIDTH_WRITE_ADDRESS) +: `BANDWIDTH_WRITE_ADDRESS];
+                    addr_bus = address_in[(send_progress*`BANDWIDTH_WRITE_ADDRESS) +: `BANDWIDTH_WRITE_ADDRESS];
                     send_progress = send_progress + 1;
-                    done = 0;
+                    done_addr = 0;
                 end
                 else if ($signed(`MEM_ADDR_SIZE-send_progress*`BANDWIDTH_WRITE_ADDRESS) > 0) begin
                     addr_bus = 0;
                     send_addr = 1;
-                    addr_bus[0 +: (`BANDWIDTH_WRITE_ADDRESS  % `BANDWIDTH_WRITE_ADDRESS)] = addr_in[send_progress*`BANDWIDTH_WRITE_ADDRESS +: (`BANDWIDTH_WRITE_ADDRESS  % `BANDWIDTH_WRITE_ADDRESS)];
+                    addr_bus[0 +: (`BANDWIDTH_WRITE_ADDRESS  % `BANDWIDTH_WRITE_ADDRESS)] = address_in[send_progress*`BANDWIDTH_WRITE_ADDRESS +: (`BANDWIDTH_WRITE_ADDRESS  % `BANDWIDTH_WRITE_ADDRESS)];
                     send_progress = send_progress + 1;
-                    done = 0;
+                    done_addr = 0;
                 end
                 else begin
-                    done = 1;
+                    done_addr = 1;
                     send_addr = 0;
                     send_progress = 0;
                 end
             end
             else begin
-                done = 0;
+                done_addr = 0;
                 send_progress = 0;
             end
         end
--- sender.v
+++ sender.v
@@ -27,41 +27,46 @@ module sender(
     input [`WORD_SIZE_BIT-1:0] data_in,
     output reg [`BANDWIDTH_WRITE_DATA-1:0] data_bus,
     output reg send_data, // to receiver module, 1 tells the receiver to start receiving
-    output reg done // to buffer or memory, 1 to indicate transmission finishs.
+    output reg done_data // to buffer or memory, 1 to indicate transmission finishs.
 );
     reg [10:0] send_progress;
-    reg [5:0] count;
+    reg [`WORD_SIZE_BIT-1:0] word_in;
     integer i,j;
     initial begin
         data_bus = 0;
         send_data = 0;
-        done = 0;
+        done_data = 0;
         send_progress = 0;
-        count = 0;
+        word_in = 0;
+    end
+    always@(data_in) begin
+        word_in = data_in;
     end
     always@(posedge clk) begin
         if (reset) begin
             send_data = 0;
-            done = 0;
+            data_bus = 0;
+            done_data = 0;
             send_progress = 0;
+            word_in = 0;
         end
         else begin
             if (send) begin
                 if($signed(`WORD_SIZE_BIT-(send_progress+1)*`BANDWIDTH_WRITE_DATA) >= 0) begin
                     send_data = 1;
-                    data_bus = data_in[(`WORD_SIZE_BIT-(send_progress+1)*`BANDWIDTH_WRITE_DATA) +: `BANDWIDTH_WRITE_DATA];
+                    data_bus = word_in[(`WORD_SIZE_BIT-(send_progress+1)*`BANDWIDTH_WRITE_DATA) +: `BANDWIDTH_WRITE_DATA];
                     send_progress = send_progress + 1;
-                    done = 0;
+                    done_data = 0;
                 end
                 else begin
-                    done = 1;
+                    done_data = 1;
                     send_data = 0;
                     send_progress = 0;
                 end
             end
             else begin
                 send_data = 0;
-                done = 0;
+                done_data = 0;
                 send_progress = 0;
             end
         end
