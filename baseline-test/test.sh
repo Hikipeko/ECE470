4061b45636b703e3819daeba43356edac51d2baa
ethanolhu <ethanolhu@gmail.com>
2023-07-07 16:49:28 +0800

    add test-batch.sh

--- baseline-test/test.sh
+++ baseline-test/test.sh
@@ -6,19 +6,23 @@ baseline_testdir="./temp-baseline"
 writebuf_testdir="./temp-writebuf"
 cd $baseline_testdir
 sed -i '1s/^/`define CONCISE\n/' testbench.v
-iverilog testbench.v -o testbench && ./testbench | uniq | sed '$d' | sed '$d' > output
+iverilog testbench.v -o testbench && ./testbench > output
+cat output | uniq | sed '$d' | sed '$d' > output-processed
 cd $pwd
 cd $writebuf_testdir
 sed -i '1s/^/`define CONCISE\n/' testbench.v
-iverilog -Wtimescale testbench.v -o testbench && ./testbench | uniq | sed '$d' | sed '$d' > output
+iverilog testbench.v -o testbench && ./testbench > output
+cat output | uniq | sed '$d' | sed '$d' > output-processed
 cd $pwd
-echo "begin diff....."
-echo "======================================"
-file1="$baseline_testdir/output"
-file2="$writebuf_testdir/output"
+# echo "begin diff....."
+# echo "======================================"
+file1="$baseline_testdir/output-processed"
+file2="$writebuf_testdir/output-processed"
 if diff -q "$file1" "$file2" >/dev/null; then
     echo "pass !!!"
+    exit 0
 else
     echo "The files are different:"
     diff "$file1" "$file2"
+    exit 1
 fi
