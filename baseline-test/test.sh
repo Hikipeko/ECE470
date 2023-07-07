861fc97ca8eb783ea3f5c0c261a53091e2a8586a
ethanolhu <ethanolhu@gmail.com>
2023-07-07 16:22:38 +0800

    test.sh working as expected :)

--- baseline-test/test.sh
+++ baseline-test/test.sh
@@ -5,7 +5,20 @@ pwd=$(pwd)
 baseline_testdir="./temp-baseline"
 writebuf_testdir="./temp-writebuf"
 cd $baseline_testdir
-iverilog testbench.v -o testbench && ./testbench > output
+sed -i '1s/^/`define CONCISE\n/' testbench.v
+iverilog testbench.v -o testbench && ./testbench | uniq | sed '$d' | sed '$d' > output
 cd $pwd
 cd $writebuf_testdir
-iverilog -Wtimescale testbench.v -o testbench && ./testbench > output
+sed -i '1s/^/`define CONCISE\n/' testbench.v
+iverilog -Wtimescale testbench.v -o testbench && ./testbench | uniq | sed '$d' | sed '$d' > output
+cd $pwd
+echo "begin diff....."
+echo "======================================"
+file1="$baseline_testdir/output"
+file2="$writebuf_testdir/output"
+if diff -q "$file1" "$file2" >/dev/null; then
+    echo "pass !!!"
+else
+    echo "The files are different:"
+    diff "$file1" "$file2"
+fi
