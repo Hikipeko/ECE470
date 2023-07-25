9cc27d76fa40ed329ed14fbd50e3d38486d01dfb
ysiad <=>
2023-07-25 22:55:13 +0800

    removed timescale

--- buffer-version/buffer.v
+++ buffer-version/buffer.v
@@ -1,23 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 19.06.2023 13:30:12
-// Design Name: 
-// Module Name: buffer
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 `include"sys_defs.vh"
 
 module buffer(
